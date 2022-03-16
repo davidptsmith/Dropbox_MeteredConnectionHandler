77ab9a78bd3a7e6496080c82450b2c6d548c2dca
David <davidptsmith@gmail.com>
2022-03-16 12:34:34 +0800

    initial commit

---
+++ Dropbox_ConnectionHandler_Console/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows;
+using Windows.Networking.Connectivity;
+
+
+
+namespace Dropbox_ConnectionHandler_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Checking Connection Type");
+            CheckIsMetered();
+            NetworkInformation.NetworkStatusChanged += (s) => CheckIsMetered();
+            
+
+            while (true)
+            {
+                Console.WriteLine("checking...");
+                if(IsMetered)
+                    closeDropbox();
+
+                Thread.Sleep(10000); 
+            }
+        }
+
+
+        private static bool isInternetAvailable;
+
+        public static bool IsInternetAvailable
+        {
+            get { return isInternetAvailable; }
+            set { isInternetAvailable = value; }
+        }
+
+        public static bool IsMetered { get; private set; }
+
+        private static void CheckIsMetered()
+        {
+            try
+            {
+
+                Console.WriteLine("Checking Connection");
+            var profile = Windows.Networking.Connectivity.NetworkInformation.GetInternetConnectionProfile();
+            IsInternetAvailable = profile != null && profile.GetNetworkConnectivityLevel() == NetworkConnectivityLevel.InternetAccess;
+
+            if (IsInternetAvailable)
+            {
+                Console.WriteLine("Connection is Available");
+                IsMetered = profile.GetConnectionCost().NetworkCostType != Windows.Networking.Connectivity.NetworkCostType.Unrestricted;
+            }
+
+
+            if (IsMetered)
+            {
+                Console.WriteLine("Connection Type Is Metered");
+                closeDropbox();
+
+            }
+            }
+            catch(Exception er)
+            {
+                var msg = er.Message;
+                Console.WriteLine("Error Closing Dropbox:");
+                Console.WriteLine(msg);
+            }
+        }
+
+
+        private static void closeDropbox()
+        {
+            try
+            {
+                Console.WriteLine("Trying to find and close Dropbox");
+                foreach (Process Proc in Process.GetProcesses())
+                    if (Proc.ProcessName.ToUpper().Equals("DROPBOX"))  //Process Excel?
+                    {
+                        Console.WriteLine("Found Dropbox");
+                        Console.WriteLine("Killing Dropbox");
+                        //Proc.Kill();
+                        Proc.Kill();
+                        Console.WriteLine("Waiting for Dropbox to exit");
+                        Proc.WaitForExit();
+                        Console.WriteLine("Dropbox Closed");
+                    }
+
+            }
+            catch (Exception er)
+            {
+                var msg = er.Message;
+                Console.WriteLine( "Error Closing Dropbox:");
+                Console.WriteLine(msg);
+
+            }
+        }
+
+
+    }
+}
---
+++ Dropbox_MeteredConnectionHandler/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.NetworkInformation;
+using Windows.Networking.Connectivity;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Dropbox_MeteredConnectionHandler
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            // check for metered conneciton
+            CheckIsMetered();
+
+            //check to see if dropbox is running
+
+            //prompt user to close dropbox
+
+            //if yes, close dropbox
+
+            if (IsMetered)
+            {
+            NetworkInformation.NetworkStatusChanged += (s) => CheckIsMetered();
+
+            //show user window 
+            InitializeComponent();
+            }
+            
+         
+        }
+
+
+        private bool isInternetAvailable;
+
+        public bool IsInternetAvailable
+        {
+            get { return isInternetAvailable; }
+            set { isInternetAvailable = value; }
+        }
+
+        public bool IsMetered { get; private set; }
+
+        private void CheckIsMetered()
+        {
+            
+
+            var profile = Windows.Networking.Connectivity.NetworkInformation.GetInternetConnectionProfile();
+            IsInternetAvailable = profile != null && profile.GetNetworkConnectivityLevel() == NetworkConnectivityLevel.InternetAccess;
+
+            if (IsInternetAvailable)
+                IsMetered = profile.GetConnectionCost().NetworkCostType != Windows.Networking.Connectivity.NetworkCostType.Unrestricted;
+            
+
+            if (IsMetered)
+            {
+                closeDropbox();
+
+            }
+        }
+
+
+        private void closeDropbox()
+        {
+            try
+            {
+                foreach (Process Proc in Process.GetProcesses())
+                    if (Proc.ProcessName.ToUpper().Contains("DROPBOX"))  //Process Excel?
+                    {
+                        //Proc.Kill();
+                        Proc.Kill(); 
+                        Proc.WaitForExit(); 
+                    }
+
+            }
+            catch (Exception er)
+            {
+                var msg = er.Message;
+                MessageBox.Show(msg, "Error Closing Dropbox"); 
+
+            }
+        }
+      
+
+        private void btn_click_closeDropbox(object sender, RoutedEventArgs e)
+        {
+            closeDropbox();
+        }
+    }
+}
